2ddc53791ff50e490ac0cd0d0b4b9112c91bb6e0
Krrish Singh <krrishjgd@gmail.com>
2026-06-03 10:47:55 +0530

    Move backend product data into separate module

--- server/server.js
+++ server/server.js
@@ -1,45 +1,8 @@
+const products = require("./productsData");
+
 const cors = require("cors");
 
 const express = require("express");
-
-const products = [
-  {
-    id: 1,
-    image: "/tshirt.png",
-    shopName: "Trends",
-    name: "Happy Tshirt",
-    price: "Rs. 300",
-    stock: 3,
-    sellerContact: "9865469865",
-    shopAddress:
-      "Subhshree Road Carriers Private Limited, 25-B, Camac St, Park Street area, Kolkata, West Bengal",
-    mapsUrl: "https://maps.app.goo.gl/6x3syBiRUjNLvito7",
-  },
-  {
-    id: 2,
-    image: "/404.png",
-    shopName: "Trends",
-    name: "404 Tshirt",
-    price: "Rs. 300",
-    stock: 0,
-    sellerContact: "9865894265",
-    shopAddress:
-      "Subhshree Road Carriers Private Limited, 25-B, Camac St, Park Street area, Kolkata, West Bengal",
-    mapsUrl: "https://maps.app.goo.gl/6x3syBiRUjNLvito7",
-  },
-  {
-    id: 3,
-    image: "/justVibes.png",
-    shopName: "Amazon",
-    name: "Just Vibes Tshirt",
-    price: "Rs. 300",
-    stock: 5,
-    sellerContact: "9865416265",
-    shopAddress:
-      "Subhshree Road Carriers Private Limited, 25-B, Camac St, Park Street area, Kolkata, West Bengal",
-    mapsUrl: "https://maps.app.goo.gl/6x3syBiRUjNLvito7",
-  },
-];
 
 const app = express();
 
